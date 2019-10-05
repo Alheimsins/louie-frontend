0a98b9e16152ed70d80781e3b646e8382c7f4d8b
maccyber <jonas.enge@gmail.com>
2019-10-05 18:19:25 +0200

    norwegian translation

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import config from '../../config'
+import no from 'vuetify/es5/locale/no'
 
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  lang: {
+    locales: { no },
+    current: 'no'
+  },
   theme: {
     themes: {
       light: { ...config.colors },
